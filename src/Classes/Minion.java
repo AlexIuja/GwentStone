e441a2014ccfe1f618fe162533dab4e3773547d9
stillscript <george.iuja@stud.acs.upb.ro>
2022-11-12 18:25:16 +0200

    Created a minion template

---
+++ src/Classes/Minion.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Minion {
+    int mana;
+    int health;
+    int attackDamage;
+    String description;
+    String colors;
+    String name;
+
+    public Minion(int mana, int health, int attackDamage, String description, String colors, String name) {
+        this.mana = mana;
+        this.health = health;
+        this.attackDamage = attackDamage;
+        this.description = description;
+        this.colors = colors;
+        this.name = name;
+    }
+}
---
+++ src/Classes/Sentinel.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class Sentinel extends Minion{
+    String type = "Sentinel";
+
+    public Sentinel(int mana, int health, int attackDamage, String description, String colors, String name) {
+        super(mana, health, attackDamage, description, colors, name);
+    }
+    
+}
